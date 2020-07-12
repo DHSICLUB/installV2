405309a4aa6ded70d19647fed5f56354c51cb0b1
DHSICLUB <56116647+DHSICLUB@users.noreply.github.com>
2020-07-12 10:10:41 +0700

    Create INSTALLERv2.py

---
+++ INSTALLERv2.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import time
+
+os.system('clear')
+time.sleep(1)
+os.system('figlet INSTALLERv2')
+time.sleep(1)
+print "****************************************************"
+time.sleep(1)
+print "*                                                  *"
+print "* Author : Mrdn441                                 *"
+time.sleep(1)
+print "* Github : https://github.com/mrdn441              *"
+print "*                                                  *"
+time.sleep(1)
+print "* gunakan script ini untuk installer kalian        *"
+print "*                                                  *"
+time.sleep(1)
+print "* Jangan disalah gunakan ya                        *"
+print "*                                                  *"
+time.sleep(1)
+print "* gunakan sebaik mungkin oke                       *"
+print "*                                                  *"
+time.sleep(1)
+print "****************************************************"
+time.sleep(1)
+print
+print "[+] Menu Pilih [+]"
+time.sleep(1)
+print "[1] Install bahan"
+time.sleep(1)
+print "[2] VIRUS"
+time.sleep(1)
+print "[3] termux-style"
+time.sleep(1)
+print "[4] TORJAN"
+time.sleep(1)
+print "[5] weeman"
+time.sleep(1)
+pilih = raw_input('[?] Ayo Dipilih : ')
+if pilih == "1":
+	          print "[+] Sedang mengginstall [+]"
+              os.system ('pkg update && pkg upgrade')
+              time.sleep(1)
+              os.system ('pkg install python2 && pkg install git')
+              time.sleep(1)
+              os.system ('pkg install nano && pkg install figlet')
+              time.sleep(1)
+              os.system ('pkg install toilet')
+              time.sleep(1)
+              os.system ('pip2 install requests mechanize')
+              os.system ('pip2 install lolcat')
+              print "[+] Penginstallan Selesai [+]"
+              time.sleep(1)
+elif pilih == "2":
+    print "[+] Sedang mengginstall [+]"
+	os.system ('git clone https://github.com/B4N954N2-ID/VIRUS')
+	print "[+]Selesai menginstall[+]"
+elif pilih == "3":
+	print "[+] Sedang mengginstall [+]"
+	os.system ('https://github.com/B4N954N2-ID/termux-style')
+	print "[+] Selesai mengginstall[+]"
+elif pilih == "4":
+	print "[+] Sedang mengginstall[+]"
+	os.system ('git clone https://github.com/ZK1Hacker/TORJAN')
+	print "[+] Selesai mengginstall[+]"
+elif pilih == "5":
+	print "[+] Sedang mengginstall [+]"
+	os.system ('git clone https://github.com/evait-security/weeman')
+	print "[+] Selesai mengginstall [+]"
