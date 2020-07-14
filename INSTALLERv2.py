67bd2c0913fd354887e034f396fc44c649024c67
DHSICLUB <56116647+DHSICLUB@users.noreply.github.com>
2020-07-15 06:51:41 +0700

    Update INSTALLERv2.py
    Baru

--- INSTALLERv2.py
+++ INSTALLERv2.py
@@ -1,103 +1,108 @@
 import os
 import sys
-import time
 
-os.system('clear')
-time.sleep(1)
-os.system('figlet INSTALLERv2')
-time.sleep(1)
-print "****************************************************"
-time.sleep(1)
-print "*                                                  *"
-print "* Author : Mrdn441                                 *"
-time.sleep(1)
-print "* Github : https://github.com/DHSICLUB             *"
-print "*                                                  *"
-time.sleep(1)
-print "* gunakan script ini untuk installer kalian        *"
-print "*                                                  *"
-time.sleep(1)
-print "* Jangan disalah gunakan ya                        *"
-print "*                                                  *"
-time.sleep(1)
-print "* gunakan sebaik mungkin oke                       *"
-print "*                                                  *"
-time.sleep(1)
-print "****************************************************"
-time.sleep(1)
-print
-print "[+] Menu Pilih [+]"
-time.sleep(1)
-print "[1] Install bahan"
-time.sleep(1)
-print "[2] VIRUS"
-time.sleep(1)
-print "[3] termux-style"
-time.sleep(1)
-print "[4] TORJAN"
-time.sleep(1)
-print "[5] weeman"
-time.sleep(1)
-print "[6] Tool-N"
-time.sleep(1)
-print "[7] TOOLSINSTALLERv4"
-time.sleep(1)
-print "[8] terkey"
-time.sleep(1)
-print "[9] exit"
-time.sleep(1)
-pilih = raw_input('[?] Ayo Dipilih : ')
-if pilih == "1":
-        print "[+] Sedang mengginstall [+]"
-        os.system ('pkg update -y && pkg upgrade -y')
-        time.sleep(1)
-        os.system ('pkg install python2 -y && pkg install git -y')
-        time.sleep(1)
-        os.system ('pkg install nano -y && pkg install figlet -y')
-        time.sleep(1)
-        os.system ('pkg install toilet -y')
-        time.sleep(1)
-        os.system ('pip2 install requests mechanize -y')
-        time.sleep(1)
-        os.system ('pip2 install lolcat')
-        time.sleep(1)
-        os.system ('pkg install curl -y && pkg install sl -y')
-        time.sleep(1)
-        os.system ('pkg install python -y && pkg install php -y')
-        time.sleep(1)
-        os.system ('pkg install bash -y')
-        print "[+] Penginstallan Selesai [+]"
-        time.sleep(1)
-elif pilih == "2":
-        print "[+] Sedang mengginstall [+]"
-        os.system ('git clone https://github.com/B4N954N2-ID/VIRUS')
-        print "[+]Selesai menginstall[+]"
-elif pilih == "3":
-        print "[+] Sedang mengginstall [+]"
-        os.system ('git clone https://github.com/B4N954N2-ID/termux-style')
-        print "[+] Selesai mengginstall[+]"
-elif pilih == "4":
-        print "[+] Sedang mengginstall[+]"
-        os.system ('git clone https://github.com/ZK1Hacker/TORJAN')
-        print "[+] Selesai mengginstall[+]"
-elif pilih == "5":
-        print "[+] Sedang mengginstall [+]"
-        os.system ('git clone https://github.com/evait-security/weeman')
-        print "[+] Selesai mengginstall [+]"
-elif pilih == "6":
-        print "[+] Sedang mengginstall [+]"
-        os.system ('git clone https://github.com/novalattasya/Tool-N')
-        print "[+] Selesai mengginstall [+]"
-        print "[+] ID : subscribe"
-        print "[+] PW : channel pajaoq"
-elif pilih == "7":
-        print "[+] Sedang mengginstall [+]"
-        os.system ('git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4')
-        print "[+] Selesai mengginstall [+]"
-elif pilih == "8":
-        print "[+] Sedang mengginstall [+]"
-        os.system ('git clone https://github.com/karjok/terkey')
-        print "[+] Selesai mengginstall [+]"
-elif pilih == "9":
-        print "[•] Sampai Jumpa [•]"
-        os.system ('exit')
+os.system ('clear')
+print "\033[0;31mHai"
+print "         @@@@@                                        @@@@@"
+print "        @@@@@@@                                      @@@@@@@"
+print "        @@@@@@@           @@@@@@@@@@@@@@@            @@@@@@@"
+print "         @@@@@@@@       @@@@@@@@@@@@@@@@@@@        @@@@@@@@"
+print "             @@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@"
+print "               @@@@@  @@@@@@@@@@@@@@@@@@@@@@@  @@@@@"
+print "                 @@  @@@@@@@@@@@@@@@@@@@@@@@@@  @@"
+print "                    @@@@@@@    @@@@@@    @@@@@@"
+print "                    @@@@@@      @@@@      @@@@@"
+print "                    @@@@@@      @@@@      @@@@@"
+print "                     @@@@@@    @@@@@@    @@@@@"
+print "                      @@@@@@@@@@@  @@@@@@@@@@"
+print "                       @@@@@@@@@@  @@@@@@@@@"
+print "                    @@   @@@@@@@@@@@@@@@@@   @@"
+print "                   @@@@  @@@@ @ @ @ @ @@@@  @@@@"
+print "                  @@@@@   @@@ @ @ @ @ @@@   @@@@@"
+print "                @@@@@      @@@@@@@@@@@@@      @@@@@"
+print "              @@@@          @@@@@@@@@@@          @@@@"
+print "           @@@@@              @@@@@@@              @@@@@"
+print "          @@@@@@@                                 @@@@@@@"
+print "           @@@@@                                   @@@@@"
+print "           +  --  --=[ User Termux Indonesia ]=--  --  +"
+pilih = raw_input('\033[1;32m[?] Masukan Password : ')
+if pilih == "QwErTyUiOpAsDfGhJklMrDn441":
+    print "password benar"
+    os.system ('clear')
+    print "\033[1;31mWelcome"
+    os.system ('figlet INSTALLERv2')
+    print "****************************************************"
+    print "*                                                  *"
+    print "* Author : DHSICLUB                                *"
+    print "* Github : https://github.com/DHSICLUB             *"
+    print "*                                                  *"
+    print "* gunakan script ini untuk installer kalian        *"
+    print "*                                                  *"
+    print "* Jangan disalah gunakan ya                        *"
+    print "*                                                  *"
+    print "* gunakan sebaik mungkin oke                       *"
+    print "*                                                  *"
+    print "****************************************************"
+    print
+    print "\033[1;34m[+] Menu Pilih [+]"
+    print "[1] Install bahan"
+    print "[2] VIRUS"
+    print "[3] termux-style"
+    print "[4] TORJAN"
+    print "[5] weeman"
+    print "[6] Tool-N"
+    print "[7] TOOLSINSTALLERv4"
+    print "[8] terkey"
+    print "[9] exit"
+    pilih = raw_input('\033[1;32m[?] Ayo Dipilih : ')
+    if pilih == "1":
+            print "[+] Sedang mengginstall [+]"
+            os.system ('pkg update -y && pkg upgrade -y')
+            os.system ('pkg install python2 -y && pkg install git -y')
+            os.system ('pkg install nano -y && pkg install figlet -y')
+            os.system ('pkg install toilet -y')
+            os.system ('pip2 install requests mechanize')
+            os.system ('pip2 install lolcat')
+            os.system ('pkg install curl -y && pkg install sl -y')
+            os.system ('pkg install python -y && pkg install php -y')
+            os.system ('pkg install bash -y')
+            print "[+] Penginstallan Selesai [+]"
+    elif pilih == "2":
+            print "tinggal COPAS"
+            print "git clone https://github.com/B4N954N2-ID/VIRUS"
+            print "cd VIRUS"
+            print "python2 virus.py"
+            print "liscense : QWIuZyBTYW56IFNuZWthbXRp"
+    elif pilih == "3":
+            print "tinggal COPAS"
+            print "git clone https://github.com/B4N954N2-ID/termux-style"
+            print "cd termux-style"
+    elif pilih == "4":
+            print "tinggal COPAS"
+            print "git clone https://github.com/ZK1Hacker/TORJAN"
+            print "cd TORJAN"
+    elif pilih == "5":
+            print "tinggal COPAS"
+            print "git clone https://github.com/evait-security/weeman"
+            print "cd weeman"
+            print "chmod +X weeman.py"
+            print "python2 weeman.py"
+    elif pilih == "6":
+            print "tinggal COPAS"
+            print "git clone https://github.com/novalattasya/Tool-N"
+            print "cd Tool-N"
+            print "python2 v1.py"
+            print "[+] ID : subscribe"
+            print "[+] PW : channel pajaoq"
+    elif pilih == "7":
+            print "tinggal COPAS"
+            print "git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4"
+            print "cd TOOLSINSTALLERv4"
+    elif pilih == "8":
+            print "git clone https://github.com/karjok/terkey"
+            print "cd terkey"
+            print "python terkey.py"
+    elif pilih == "10":
+            print "tinggal COPAS"
+            print "git clone https://github.com/B4N954N2-ID/marshal"
+            print "cd marshal"
